764fe9eca65227be36f634932243c62480c0e7f4
... <...>
2015-05-15 13:59:49 +0100

    ...

--- MatrixTesterApp/main.cpp
+++ MatrixTesterApp/main.cpp
@@ -1,11 +1,14 @@
 #include "matrixtesterapp.h"
 #include <QtWidgets/QApplication>
+
 #include "../Library/studio.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
 	MatrixTesterApp w;
 	w.show();
+
 	return a.exec();
 }
