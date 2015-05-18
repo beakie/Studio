cfd99f62caddaf80f147183f79b3d823879310e8
... <...>
2015-05-18 10:46:35 +0100

    ...

--- Play2d/skeletalrender.h
+++ Play2d/skeletalrender.h
@@ -2,12 +2,22 @@
 #define SKELETALRENDER_H
 
 #include <qwidget.h>
+#include <qgraphicswidget.h>
+#include <qpainter.h>
+
+#include "../Library/movement.h"
 
 class SkeletalRender : public QWidget
 {
 
 public:
+	Movement::Skeletal2d* Skeletal;
 
+	SkeletalRender(Movement::Skeletal2d* skeletal);
+
+	~SkeletalRender();
+
+	void paintEvent(QPaintEvent *event);
 
 };
 
