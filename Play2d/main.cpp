fe4d25ecad7efb4dc2bc9b7304682c591b609a56
... <...>
2015-05-18 16:28:35 +0100

    ....

--- Play2d/main.cpp
+++ Play2d/main.cpp
@@ -14,13 +14,15 @@ int main(int argc, char *argv[])
 	skeletal.ZeroPositions.Positions[2]->operator=({ 200, 0 });
 	skeletal.ZeroPositions.Positions[3]->operator=({ 300, 0 });
 
-	int jointIndex1 = 1;
-	double jointRotation1 = Common::radian<double>(10.0);
-	skeletal.Joints.Joints[jointIndex1]->set(Space2d::getRotationMatrix(jointRotation1, skeletal.ZeroPositions.Positions[jointIndex1]->Values[0], skeletal.ZeroPositions.Positions[jointIndex1]->Values[1]));
+	Common::List<double> rotationList = Common::List<double>();
+	rotationList.add(0);
+	rotationList.add(10);
+	rotationList.add(10);
+	rotationList.add(0);
 
-	int jointIndex2 = 2;
-	double jointRotation2 = Common::radian<double>(10.0);
-	skeletal.Joints.Joints[jointIndex2]->set(Space2d::getRotationMatrix(jointRotation2, skeletal.ZeroPositions.Positions[jointIndex2]->Values[0], skeletal.ZeroPositions.Positions[jointIndex2]->Values[1]));
+	for (int i = 0; i < rotationList.count(); i++)
+		if (rotationList.Items[i] != 0)
+			skeletal.Joints.Joints[i]->set(Space2d::getRotationMatrix(Common::radian<double>(rotationList.Items[i]), skeletal.ZeroPositions.Positions[i]->Values[0], skeletal.ZeroPositions.Positions[i]->Values[1]));
 
 	SkeletalRender skeletalRender(&skeletal);
 	skeletalRender.show();
