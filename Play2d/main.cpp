e5a5cd1ad46a741602f2dd99e2df3a8e791793e0
... <...>
2015-05-18 12:26:09 +0100

    dodgy

--- Play2d/main.cpp
+++ Play2d/main.cpp
@@ -15,6 +15,8 @@ int main(int argc, char *argv[])
 	skeletal.ZeroPositions.Positions[2]->operator=({ 200, 0 });
 	skeletal.ZeroPositions.Positions[3]->operator=({ 300, 0 });
 
+	skeletal.Joints.Joints[2]->set(Space2d::getRotationMatrix(-1.57079633));
+
 	SkeletalRender skeletalRender(&skeletal);
 
 	skeletalRender.show();
