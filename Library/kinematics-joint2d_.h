331519f72630237d1777dc1202bdff7584c283a1
... <...>
2015-05-19 10:36:12 +0100

    .

--- Library/kinematics-joint2d_.h
+++ Library/kinematics-joint2d_.h
@@ -14,7 +14,7 @@ namespace Kinematics
 	}
 
 	template <typename TPRECISION>
-	TPRECISION calculateJointAngleForDistance(const TPRECISION length1, const TPRECISION length2)
+	TPRECISION calculateAngleToAlign(const TPRECISION length1, const Space2d::LineSegment2d<TPRECISION> lineSegment)
 	{
 		//Common::cos<TPRECISION>()
 		return 0.0;
