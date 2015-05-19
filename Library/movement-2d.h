19b519c5aeb209514dfdce38cb617b5a8fe6ec42
... <...>
2015-05-19 09:38:06 +0100

    .

---
+++ Library/movement-2d.h
@@ -0,0 +1,33 @@
+#ifndef MOVEMENT2D_H
+#define MOVEMENT2D_H
+
+#include "space2d.h"
+
+#include "movement-bonemap.h"
+#include "movement-jointlist.h"
+#include "movement-positiontranslation2d_.h"
+#include "movement-positionlist.h"
+#include "movement-skeletal.h"
+
+namespace Movement
+{
+
+	typedef JointList<Common::Matrix3F32> JointList2d32;
+	typedef JointList<Common::Matrix3F64> JointList2d64;
+	typedef JointList<Common::Matrix3F> JointList2d;
+
+	typedef PositionList<Space2d::PlotF32> PositionList2d32;
+	typedef PositionList<Space2d::PlotF64> PositionList2d64;
+	typedef PositionList<Space2d::PlotF> PositionList2d;
+
+	typedef PositionList<Space2d::PlotF32> JointPositionList2d32;
+	typedef PositionList<Space2d::PlotF64> JointPositionList2d64;
+	typedef PositionList<Space2d::PlotF> JointPositionList2d;
+
+	typedef Skeletal<Space2d::PlotF32, Common::Matrix3F32> Skeletal2d32;
+	typedef Skeletal<Space2d::PlotF64, Common::Matrix3F64> Skeletal2d64;
+	typedef Skeletal<Space2d::PlotF, Common::Matrix3F> Skeletal2d;
+
+}
+
+#endif // MOVEMENT2D_H
---
+++ Library/movement-3d.h
@@ -0,0 +1,33 @@
+#ifndef MOVEMENT3D_H
+#define MOVEMENT3D_H
+
+#include "space3d.h"
+
+#include "movement-bonemap.h"
+#include "movement-jointlist.h"
+#include "movement-positiontranslation3d_.h"
+#include "movement-positionlist.h"
+#include "movement-skeletal.h"
+
+namespace Movement
+{
+
+	typedef JointList<Common::Matrix4F32> JointList3d32;
+	typedef JointList<Common::Matrix4F64> JointList3d64;
+	typedef JointList<Common::Matrix4F> JointList3d;
+
+	typedef PositionList<Space3d::CoordinateF32> PositionList3d32;
+	typedef PositionList<Space3d::CoordinateF64> PositionList3d64;
+	typedef PositionList<Space3d::CoordinateF> PositionList3d;
+
+	typedef PositionList<Space3d::CoordinateF32> JointPositionList3d32;
+	typedef PositionList<Space3d::CoordinateF64> JointPositionList3d64;
+	typedef PositionList<Space3d::CoordinateF> JointPositionList3d;
+
+	typedef Skeletal<Space3d::CoordinateF32, Common::Matrix4F32> Skeletal3d32;
+	typedef Skeletal<Space3d::CoordinateF64, Common::Matrix4F64> Skeletal3d64;
+	typedef Skeletal<Space3d::CoordinateF, Common::Matrix4F> Skeletal3d;
+
+}
+
+#endif // MOVEMENT3D_H
