3026537d796d3e28754ef018fd38cd23f7f5e8fb
... <...>
2015-05-22 09:36:27 +0100

    .

--- Library/kinematics-arm2link_.cpp
+++ Library/kinematics-arm2link_.cpp
@@ -4,22 +4,13 @@ void Kinematics::calculateJointAngleForDistance(const double boneLength1, const 
 {
 	if ((boneLength1 + boneLength2) < distance)
 	{
-		angle = 0;
 		isReachable = false;
+		angle = 0;
 		return;
 	}
-	else
-		isReachable = true;
 
-	double boneLength1DistancePercentage = boneLength1 / (boneLength1 + boneLength2);
-
-	double boneLength1Opposite = distance * boneLength1DistancePercentage;
-
-	//double boneLength1Adjacent = Common::power((boneLength1Hypotenuse * boneLength1Hypotenuse) - (boneLength1 * boneLength1), 0.5);
-
-	double boneLength1Angle = Common::sin<double>(boneLength1Opposite / boneLength1);
-
-	angle = boneLength1Angle * 2;
+	isReachable = true;
+	angle = Common::sin<double>((distance * (boneLength1 / (boneLength1 + boneLength2))) / boneLength1) * 2;
 }
 
 bool Kinematics::alignRotatingArmWithCoordinate(const double bone1Length, const double bone2Length, double& rotatingJointAngle, double& reach1JointAngle, double& reach2JointAngle)
