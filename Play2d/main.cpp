567f14324f673500bf123fa7930b87cf0518835a
... <...>
2015-05-22 13:21:11 +0100

    .

--- Play2d/main.cpp
+++ Play2d/main.cpp
@@ -8,30 +8,25 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 
-	Movement::Skeletal2d skeletal(5);
+	Movement::Skeletal2d skeletal(3);
 
-	skeletal.ZeroPositions.Positions[0]->operator=({ 100, 0 });
-	skeletal.ZeroPositions.Positions[1]->operator=({ 200, 0 });
-	skeletal.ZeroPositions.Positions[2]->operator=({ 300, 0 });
-	skeletal.ZeroPositions.Positions[3]->operator=({ 400, 0 });
-	skeletal.ZeroPositions.Positions[4]->operator=({ 500, 0 });
+	skeletal.ZeroPositions.Positions[0]->operator=({ 50, 0 });
+	skeletal.ZeroPositions.Positions[1]->operator=({ 100, 0 });
+	skeletal.ZeroPositions.Positions[2]->operator=({ 150, 0 });
 
 	bool isReachable;
 	double flexAngleRadian;
-	Kinematics::calculateAngleForFlexJointDistance(100, 100, 50, flexAngleRadian, isReachable);
+	Kinematics::calculateAngleForFlexJointDistance(50, 50, 50, flexAngleRadian, isReachable);
 	double flexAngleDegree = Common::degree(flexAngleRadian);
 
 	double rotateAngleRadian;
-	Kinematics::calculateAlignmentForFlexJointDistance(100, 100, 50, rotateAngleRadian);
+	Kinematics::calculateAlignmentForFlexJointDistance(50, 50, 50, rotateAngleRadian);
 	double rotateAngleDegree = Common::degree(rotateAngleRadian);
 
 	Common::List<double> rotationList = Common::List<double>();
-	rotationList.add(5);
-	rotationList.add(5);
-	rotationList.add(5);
-	rotationList.add(5);
-	//	rotationList.add(180 - rotateAngleDegree);
-//	rotationList.add(180 - flexAngleDegree);
+	rotationList.add(0);
+//	rotationList.add(180 - rotateAngleDegree);
+	rotationList.add(180 - flexAngleDegree);
 
 	for (int i = 0; i < rotationList.count(); i++)
 		if (rotationList.Items[i] != 0)
