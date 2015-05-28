db71b44c268593e89f2f9f357bfe8480d883fec3
... <...>
2015-05-28 09:46:24 +0100

    ...

--- Library/movement-bonefixedpositionlist.h
+++ Library/movement-bonefixedpositionlist.h
@@ -9,6 +9,7 @@ namespace Movement
 	template <typename TPOINT>
 	struct BoneFixedPositionList
 	{
+		UInt8* BoneIndex;
 		TPOINT** Positions;
 		UInt8 PositionCount;
 
@@ -21,10 +22,14 @@ namespace Movement
 		template <typename TVALUE = FloatMax>
 		BoneFixedPositionList(const BoneFixedPositionList<TPOINT>& boneFixedPositionList)
 		{
+			BoneIndex = new UInt8[boneFixedPositionList.PositionCount];
 			Positions = new TPOINT*[boneFixedPositionList.PositionCount];
 
 			for (UInt8 i = 0; i < boneFixedPositionList.PositionCount; i++)
+			{
+				BoneIndex[i] = boneFixedPositionList.BoneIndex[i];
 				Positions[i] = new TPOINT(*boneFixedPositionList.Positions[i]);
+			}
 
 			PositionCount = boneFixedPositionList.PositionCount;
 		}
@@ -32,21 +37,31 @@ namespace Movement
 		BoneFixedPositionList(UInt8 size)
 			: PositionCount(size)
 		{
+			BoneIndex = new UInt8[size];
 			Positions = new TPOINT*[size];
 
 			for (UInt8 i = 0; i < size; i++)
+			{
+				BoneIndex[i] = 0;
 				Positions[i] = new TPOINT(TPOINT::getZero());
+			}
 		}
 
 		BoneFixedPositionList<TPOINT>& operator=(const BoneFixedPositionList<TPOINT>& boneFixedPositionList)
 		{
+			UInt8* tmpBoneIndex = new UInt8[boneFixedPositionList.PositionCount + 1];
 			TPOINT** tmpPositions = new TPOINT*[boneFixedPositionList.PositionCount + 1];
 
 			for (UInt8 i = 0; i < boneFixedPositionList.PositionCount; i++)
+			{
+				tmpBoneIndex[i] = boneFixedPositionList.BoneIndex[i];
 				tmpPositions[i] = new TPOINT(*boneFixedPositionList.Positions[i]);
+			}
 
+			delete[] BoneIndex;
 			delete[] Positions;
 
+			BoneIndex = tmpBoneIndex;
 			Positions = tmpPositions;
 
 			PositionCount = boneFixedPositionList.PositionCount;
@@ -56,15 +71,22 @@ namespace Movement
 
 		UInt8 addPoint()
 		{
+			UInt8* tmpBoneIndex = new UInt8[PositionCount + 1];
 			TPOINT** tmpPositions = new TPOINT*[PositionCount + 1];
 
 			for (UInt8 i = 0; i < PositionCount; i++)
+			{
+				tmpBoneIndex[i] = BoneIndex[i];
 				tmpPositions[i] = Positions[i];
+			}
 
+			tmpBoneIndex[PositionCount] = 0;
 			tmpPositions[PositionCount] = new TPOINT(TPOINT::getZero());
 
+			delete[] BoneIndex;
 			delete[] Positions;
 
+			BoneIndex = tmpBoneIndex;
 			Positions = tmpPositions;
 
 			PositionCount++;
@@ -74,6 +96,8 @@ namespace Movement
 
 		~BoneFixedPositionList()
 		{
+			delete[] BoneIndex;
+
 			for (UInt8 i = 0; i < PositionCount; i++)
 				delete Positions[i];
 
--- Library/movement-skeletal.h
+++ Library/movement-skeletal.h
@@ -5,7 +5,7 @@
 #include "common.h"
 
 #include "movement-bonemap.h"
-#include "movement-bonepositionlist.h"
+#include "movement-bonefixedpositionlist.h"
 #include "movement-positionlist.h"
 #include "movement-jointlist.h"
 
@@ -18,7 +18,7 @@ namespace Movement
 		BoneMap BoneMap;
 		PositionList<TPOINT> ZeroPositions; // This should be the position of the axis in 3d space. Not the bone ends!
 		// There could be a representation of the bones but zero positions shouldnt be it. it only have a use when rendering the skeletal. Do i need skeletalrender?
-
+		BoneFixedPositionList<TPOINT> EndEffectors;
 
 		Skeletal()
 		{
