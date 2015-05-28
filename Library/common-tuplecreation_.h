da7a9b5373dcd7a3299e632a813082995505c906
... <...>
2015-05-28 10:45:08 +0100

    .

---
+++ Library/common-tuplecreation_.h
@@ -0,0 +1,15 @@
+#ifndef COMMONTUPLECREATION_H
+#define COMMONTUPLECREATION_H
+
+#include "core.h"
+
+namespace Common
+{
+	template <typename T1 = FloatMax>
+	
+	{
+		T1 Item1;
+	};
+}
+
+#endif // COMMONTUPLECREATION_H
