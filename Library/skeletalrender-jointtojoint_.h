9b5276f75017b2437da4cdc90582dac2ad9dfa65
... <...>
2015-05-29 14:59:30 +0100

    .

--- Library/skeletalrender-jointtojoint_.h
+++ Library/skeletalrender-jointtojoint_.h
@@ -12,9 +12,7 @@ namespace SkeletalRender
 		Common::ManagedList<Space2d::LineSegment2d<TVALUE>, UInt8> boneList = Common::ManagedList<Space2d::LineSegment2d<TVALUE>, UInt8>();
 
 		for (UInt8 i = 0; i < boneMap.BoneCount; i++)
-			if (boneMap.ParentBones[i] == i)
-				boneList.add({ Common::Vector2<TVALUE>::getZero(), *positionList.Positions[i] });
-			else
+			if (boneMap.ParentBones[i] != i)
 				boneList.add({ *positionList.Positions[boneMap.ParentBones[i]], *positionList.Positions[i] });
 
 		return boneList;
@@ -25,10 +23,8 @@ namespace SkeletalRender
 	{
 		Common::ManagedList<Space3d::LineSegment3d<TVALUE>, UInt8> boneList = Common::ManagedList<Space3d::LineSegment3d<TVALUE>, UInt8>();
 
-		for (UInt8 i = 0; i < boneMap.BoneCount; i++)
-			if (boneMap.ParentBones[i] == i)
-				boneList.add({ Common::Vector3<TVALUE>::getZero(), *positionList.Positions[i] });
-			else
+		for (UInt8 i = 0; i < boneMap.BoneCount - 1; i++)
+			if (boneMap.ParentBones[i] != i)
 				boneList.add({ *positionList.Positions[boneMap.ParentBones[i]], *positionList.Positions[i] });
 
 		return boneList;
