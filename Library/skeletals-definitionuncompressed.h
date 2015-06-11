0619c8af8d87e222e1d12b3cc13d733fe2d5a6bb
... <...>
2015-06-11 15:44:50 +0100

    .

--- Library/skeletals-definitionuncompressed.h
+++ Library/skeletals-definitionuncompressed.h
@@ -1,6 +1,8 @@
 #ifndef SKELETALSDEFINITIONUNCOMPRESSED_H
 #define SKELETALSDEFINITIONUNCOMPRESSED_H
 
+#include "movement-jointlist.h"
+
 #include "skeletals-definition.h"
 
 namespace Skeletals
@@ -8,8 +10,11 @@ namespace Skeletals
 	template <typename TPOINT, typename TMATRIX>
 	struct DefinitionUncompressed : Definition<TPOINT, TMATRIX>
 	{
+		Movement::JointList<TMATRIX> Joints;
+		Movement::JointList<TMATRIX> JointPlacement;
 
 		DefinitionUncompressed()
+			: base()
 		{
 		}
 
