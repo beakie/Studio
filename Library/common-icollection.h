b2ebc2a46da14cdb11221de42ba7de793429bd22
... <...>
2015-06-16 14:08:52 +0100

    .

---
+++ Library/common-icollection.h
@@ -0,0 +1,89 @@
+#ifndef COMMON_COLLECTION_H
+#define COMMON_COLLECTION_H
+
+namespace Common
+{
+
+	template <typename TVALUE, typename TINDEX = UIntMax>
+	struct Collection
+	{
+
+	protected:
+		TINDEX Count;
+
+	public:
+		TVALUE* Items;
+
+		Collection()
+		{
+			Count = 0;
+
+			Items = 0;
+		}
+
+		Collection(TINDEX count, TVALUE default)
+		{
+			Count = count;
+
+			Items = new TVALUE[count];
+
+			for (TINDEX i = 0; i < Count; i++)
+				Items[i] = default;
+		}
+
+		Collection(const Collection<TVALUE, TINDEX>& collection)
+		{
+			Items = 0;
+			operator=(collection);
+		}
+
+		TINDEX count() const
+		{
+			return Count;
+		}
+
+		TINDEX capacity() const
+		{
+			return Count;
+		}
+
+		Collection<TVALUE, TINDEX>& operator=(const Collection<TVALUE, TINDEX> &collection)
+		{
+			if (this != &collection)
+			{
+				TVALUE* tmp = new TVALUE[collection.capacity()];
+				Count = collection.Count;
+
+				for (TINDEX i = 0; i < Count; i++)
+					tmp[i] = collection.Items[i];
+
+				delete[] Items;
+
+				Items = tmp;
+			}
+
+			return *this;
+		}
+
+		TVALUE & operator[] (TINDEX n)
+		{
+			return Items[n];
+		}
+
+		bool exists(const TVALUE value)
+		{
+			for (TINDEX i = 0; i < Count; i++)
+				if (Items[i] == value)
+					return true;
+
+			return false;
+		}
+
+		~Collection() {
+			delete[] Items;
+		}
+	};
+
+}
+
+#endif // COMMON_COLLECTION_H
