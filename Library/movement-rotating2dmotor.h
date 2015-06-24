b45c64b16a11ba894bcbe9f83e763a3de8af65be
... <...>
2015-06-24 14:16:50 +0100

    .

---
+++ Library/movement-rotating2dmotor.h
@@ -0,0 +1,31 @@
+#ifndef MOVEMENT_LINEAR2DACTUATOR_H
+#define MOVEMENT_LINEAR2DACTUATOR_H
+
+#include "space2d-plot.h"
+#include "space2d-transformmatrixbuilders_.h"
+#include "movement-iactuator.h"
+
+namespace Movement
+{
+	template <typename TVALUE>
+	class Linear2dActuator : IActuator<Common::Matrix3<TVALUE>> // Translates along X axis
+	{
+	public:
+		TVALUE Translation;
+			
+		Linear2dActuator()
+		{
+		}
+
+		void set(Common::Matrix3<TVALUE>& matrix)
+		{
+			matrix = Space2d::getTranslationMatrix(Translation, 0);
+		}
+
+		virtual ~Linear2dActuator()
+		{
+		}
+	};
+}
+
+#endif // MOVEMENT_LINEAR2DACTUATOR_H
